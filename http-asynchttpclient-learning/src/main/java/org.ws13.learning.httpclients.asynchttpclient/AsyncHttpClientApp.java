53736649b24558640cc31c2fff37ce567d3205f6
ctranxuan <ctranxuan@gmail.com>
2016-03-04 17:11:02 +0100

    fix(interval): with interval, bug
    - the issue comes from the Future of the async client and the way RxJava is assuming the cancel

--- http-asynchttpclient-learning/src/main/java/org.ws13.learning.httpclients.asynchttpclient/AsyncHttpClientApp.java
+++ http-asynchttpclient-learning/src/main/java/org.ws13.learning.httpclients.asynchttpclient/AsyncHttpClientApp.java
@@ -5,6 +5,11 @@ import org.asynchttpclient.DefaultAsyncHttpClientConfig;
 import org.asynchttpclient.ListenableFuture;
 import org.asynchttpclient.Response;
 import rx.Observable;
+import rx.Subscriber;
+import rx.subscriptions.Subscriptions;
+
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.TimeUnit;
 
 import static org.asynchttpclient.Dsl.asyncHttpClient;
 import static org.asynchttpclient.Dsl.config;
@@ -24,18 +29,40 @@ public class AsyncHttpClientApp {
                 .setMaxConnections(50)
                 .setMaxConnectionsPerHost(50);
 
-        ListenableFuture<Response> response;
-        response = asyncHttpClient(config).prepareGet(url).execute(new AsyncCompletionHandler<Response>() {
+
+        Observable<Response> responseObservable = Observable.create(new Observable.OnSubscribe<Response>() {
             @Override
-            public Response onCompleted(final Response aResponse) throws Exception {
-                System.out.println("AsyncHttpClientApp.onCompleted: " + aResponse.getResponseBody());
-                return aResponse;
+            public void call(final Subscriber<? super Response> aSubscriber) {
+                ListenableFuture<Response> response;
+                response = asyncHttpClient(config).prepareGet(url).execute(new AsyncCompletionHandler<Response>() {
+                    @Override
+                    public Response onCompleted(final Response aResponse) throws Exception {
+                        System.out.println("AsyncHttpClientApp.onCompleted: " + aResponse.getResponseBody());
+                        aSubscriber.onNext(aResponse);
+                        aSubscriber.onCompleted();
+                        return aResponse;
+                    }
+
+//                    @Override
+//                    public void onThrowable(final Throwable t) {
+//                        if (t instanceof CancellationException) {
+//                            // silent cath due to miscompatibility between rxjava and asynchttpclient
+//                            // see https://github.com/ReactiveX/RxJava/issues/1022
+//                        } else {
+//                            super.onThrowable(t);
+//
+//                        }
+//                    }
+                });
+
             }
         });
 
-        Observable.from(response)
+        Observable.interval(5, TimeUnit.SECONDS)
+                .flatMap(l -> responseObservable)
                 .toBlocking()
-                .subscribe(System.out::println);
+                .subscribe(System.out::println)
+        ;
 
     }
 }
